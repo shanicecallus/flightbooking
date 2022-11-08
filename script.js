dc757ae4262db46720b6d2e4ff2b84fbcf780996
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2022-11-08 10:54:05 +0100

    swiper amended, not functional

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ menu.onclick = () =>{
 };
 
 var swiper = new Swiper(".home-slider", {
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
