a698191f18c83c428e5db4169735394043b665b5
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2022-12-02 09:33:32 +0100

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,17 @@
+<?php
+    include_once'header.php'
+?>
+
+<style>
+    <?php
+        include 'CSS/style.css';
+    ?>
+</style>
+
+<!-- My profile code starts here -->
+
+<!-- My profile code ends here -->
+
+<?php
+    include_once'footer.php'
+?>
