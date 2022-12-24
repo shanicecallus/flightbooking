cf31df3b3c59fe39b2f53175d0a9fe294457fbe0
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2022-12-24 11:15:02 +0100

    Prints email when logged in.
    Prints out all emails of all users when checking the my profile section of a logged in user. Needs fixing.

--- profile.php
+++ profile.php
@@ -1,5 +1,9 @@
 <?php
-    include_once'header.php'
+include_once 'includes/dbh.inc.php';
+?>
+
+<?php
+include_once 'header.php';
 ?>
 
 <style>
@@ -9,7 +13,19 @@
 </style>
 
 <!-- My profile code starts here -->
+<body>
+    <?php
+    $sql = "select * from users;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
 
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['email'];
+        }
+    }
+    ?>
+</body>
 <!-- My profile code ends here -->
 
 <?php
