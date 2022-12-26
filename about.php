7acc627e9bfdea2e5334a168249a717918edb739
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2022-12-26 19:52:48 +0100

    About Page Updated

--- about.php
+++ about.php
@@ -11,6 +11,34 @@
 
 <img class="headerimg" src="images/Berlin.jpg" alt="header" width=100% height="400">
 
+<!-- About Section Starts Here  -->
+<section class="about">
+    <div class="content">
+        <h2>About Us</h2>
+        <p><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex sed massa consequat laoreet. Sed pharetra porta felis et gravida. Integer efficitur lectus quis quam molestie, nec eleifend elit posuere. Etiam id consequat ipsum, at malesuada velit. Suspendisse in magna eget augue maximus rhoncus. Sed auctor vestibulum tellus, in condimentum sapien convallis non. Morbi vel ultrices ex, nec finibus nibh. Nullam convallis mattis justo id suscipit. Morbi consectetur lectus a metus feugiat, eu faucibus nisl finibus. Sed ante tortor, consequat tempus dui in, feugiat auctor ex. Aenean consectetur vel leo eget faucibus. Curabitur aliquam dolor sed tortor gravida convallis. Morbi eu nulla risus. <br>
+
+        <br>Etiam vestibulum quis turpis vel pulvinar. Ut tincidunt arcu id nulla tincidunt gravida. Curabitur ipsum purus, posuere eget varius at, hendrerit sit amet magna. Phasellus suscipit libero quis placerat venenatis. Maecenas imperdiet aliquam magna id sodales. Pellentesque dui felis, blandit eget mattis nec, facilisis vel elit. Pellentesque eu odio id lectus euismod convallis. Aenean id est lectus. Fusce sagittis aliquet est, imperdiet sollicitudin lectus commodo a. Fusce ultricies, risus et volutpat pellentesque, mauris massa sodales elit, vitae vulputate ipsum nunc nec ex. Etiam dapibus congue nisi, ut eleifend tortor vehicula vel. Aliquam porta auctor mauris, vitae posuere ex imperdiet ac. Donec consectetur convallis leo, sit amet faucibus mi sodales id. Nullam et nulla consequat, egestas risus vitae, faucibus leo. Nam rutrum vel magna at feugiat.
+    </p>
+    <div class="icons-container">
+        <div class="icons">
+            <i class="fas fa-map">
+                <span>Top Destinations</span>
+            </i>
+            </div>
+            <div class="icons">
+            <i class="fas fa-hand-holding-usd">
+                <span>Affordable Prices</span>
+            </i>
+            </div>
+            <div class="icons">
+            <i class="fas fa-headset">
+                <span>Customer Service</span>
+            </i> 
+    </div>   
+    </div>
+</section>
+<!-- About Section Ends Here -->
+
 <?php
     include_once'footer.php'
 ?>
