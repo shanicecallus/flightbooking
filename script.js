261da09da677eea03ba61c63ec9af89b4434679d
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2022-12-26 20:12:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,3 @@ var swiper = new Swiper(".home-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-const stripe = Stripe("pk_test_51MGjlgErwAmkvHMKhNOhSNXa6PaEnCx2elLGyOJdMMOy72Nw53xn0APBRxxpB7Gr9CNXmTzBov7s7On1iB2JCV7V001WUc7HU4")
-const btn = document.querySelector('#btn')
-btn.addEventListener('click', ()=>{
-  fetch('/checkout.php', {
-    method:"POST", 
-    header:{
-      'Content-Type' : 'application/json', 
-    }, 
-    body: JSON.stringify({})
-  }).then(res=> res.json())
-  .then(payload => {
-    stripe.redirectToCheckout({sessionId: payload.id})
-  })
-})
