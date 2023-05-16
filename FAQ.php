25ca7dd0f01440ebd32a60ee8d1857395136b574
shanicecallus <73163856+shanicecallus@users.noreply.github.com>
2023-05-16 10:27:37 +0200

    adding FAQ section

---
+++ FAQ.php
@@ -0,0 +1,37 @@
+<?php
+    include_once'header.php'
+?>
+
+<style>
+    <?php
+        include 'CSS/style.css';
+    ?>
+</style>
+
+<section>
+
+    <h1>About the Website </h1>
+    <p>
+        This website allows a user to sign up, log in and place an online booking for flights. Detailed information about this website including ratings and reviews are available in the <a href="about.php"> About </a> page. 
+    </p>
+
+
+    <h1>
+        Placing your online booking
+    </h1>
+    <p>
+        On this website you may fill in a form with your details on the <a href='book.php'> Booking </a> page. 
+    </p>
+
+    <h1>
+        How can do I sign up?
+    </h1>
+    <p>
+        By visiting the <a href='signup.php'> Sign Up </a> page a user can input his credentials and be able to use them in the log in page later when logging in. 
+    </p>
+
+</section>
+
+<?php
+    include_once'footer.php'
+?>
